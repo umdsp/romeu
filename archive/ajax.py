57c72f9dce0fa97771038828209a220aa48dbdbc
Kevin Zurawel <kzurawel@gmail.com>
2011-09-20 15:44:30 -0400

    Adding Dajax call for stages

---
+++ archive/ajax.py
@@ -0,0 +1,14 @@
+from dajax.core import Dajax
+from dajaxice.decorators import dajaxice_register
+from archive.models import Location, Stage
+
+@dajaxice_register
+def updatestages(request, option):
+    dajax = Dajax()
+    loc = Location.objects.get(option)
+    stages = Stage.objects.filter(venue=loc)
+    out = "<option value selected='selected'>--------------</option>"
+    for stage in stages:
+        out += "<option value='%d'>%s</option>" % (stage.pk, stage.__unicode__())
+    dajax.assign('#id_stage', 'innerHTML', out)
+    return dajax.json()
