cbaba4baa56cbf3b3ca2236baa7bcd5492b3f183
Julio Perez <perezbjc@gmail.com>
2014-02-13 19:00:31 +0000

    [CTDA-109] added search_fields option and year hierarchy

--- archive/admin.py
+++ archive/admin.py
@@ -699,11 +699,11 @@ class AwardAdmin(TranslationAdmin):
 
 class AwardCandidateAdmin(TranslationAdmin):
     form = arcforms.AwardCandidateAdminForm
-    
+    search_fields = ['award__title', 'category', 'recipient__creator_name']
     verbose_name = "award nomination / win"
     verbose_name_plural = "award nominations / wins"
     list_filter = ('has_attention', 'year')
-    list_display = ('award', 'year', 'category')
+    list_display = ('award', 'year', 'category', 'recipient')
     ordering = ['award', '-year']
 
     def __init__(self, model, admin_site):
